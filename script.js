d8660a655a81a2a858abff44589538537a5d1c52
m9t3zhn1k <m9t3zhn1k@mail.ru>
2022-05-12 19:38:10 +0300

    change logic of burger menu

--- script.js
+++ script.js
@@ -2,21 +2,31 @@ const menuIcon = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
 const menuList = document.querySelector('.menu__list');
 const menuLinks = document.querySelectorAll('.menu__item');
+let isMenuOpened = false;
 
 menuIcon.addEventListener('click', openMenu);
 menuLinks.forEach(link => link.addEventListener('click', openMenu));
+document.addEventListener('click', checkClick);
 
 function openMenu() {
-    document.body.classList.toggle('_lock');
-    menuIcon.classList.toggle('_active');
-    menuBody.classList.toggle('_active');
-    menuList.classList.toggle('_active');
-}
-
-document.addEventListener('click', function(e) {
-    if ((e.target != (menuIcon || menuLinks)) && document.body.classList.contains('_lock')) {
+    if (!isMenuOpened) {
+        document.body.classList.add('_lock');
+        menuIcon.classList.add('_active');
+        menuBody.classList.add('_active');
+        menuList.classList.add('_active');
+        isMenuOpened = true;
+    } else {
         document.body.classList.remove('_lock');
         menuIcon.classList.remove('_active');
         menuBody.classList.remove('_active');
+        menuList.classList.remove('_active');
+        isMenuOpened = false;
+    }
+
+}
+
+function checkClick() {
+    if (event.target == menuBody) {
+        openMenu();
     };
-});
+}
