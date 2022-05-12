11a3c13b8ceae8fb513534c48bad4825fb70f8df
m9t3zhn1k <m9t3zhn1k@mail.ru>
2022-05-12 18:59:50 +0300

    change responsive in style.css

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const menuIcon = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
+const menuList = document.querySelector('.menu__list');
 const menuLinks = document.querySelectorAll('.menu__item');
 
+menuIcon.addEventListener('click', openMenu);
+menuLinks.forEach(link => link.addEventListener('click', openMenu));
 
-menuIcon.addEventListener('click', (e) => toggle());
-menuLinks.forEach(link => link.addEventListener('click', (e) => toggle()));
-
-
-function toggle() {
+function openMenu() {
     document.body.classList.toggle('_lock');
     menuIcon.classList.toggle('_active');
     menuBody.classList.toggle('_active');
+    menuList.classList.toggle('_active');
 }
 
 document.addEventListener('click', function(e) {
-    if ( (e.target != (menuIcon || menuLinks)) && document.body.classList.contains('_lock')) {
+    if ((e.target != (menuIcon || menuLinks)) && document.body.classList.contains('_lock')) {
         document.body.classList.remove('_lock');
         menuIcon.classList.remove('_active');
         menuBody.classList.remove('_active');
